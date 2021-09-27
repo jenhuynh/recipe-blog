3e1d284e07e6cba89aaf223efab0e34b9232aaf1
jenhuynh <jendoubleh@gmail.com>
2021-09-27 15:38:50 -0700

    Add new component recipet two

--- app/src/App.js
+++ app/src/App.js
@@ -2,20 +2,23 @@ import * as React from "react";
 
 import { Routes, Route, Link } from "react-router-dom";
 
-import RecipeOne from "./RecipeOne";
-import Tasks from "./Tasks";
+import RecipeTwo from "./RecipeTwo";
+import Recipes from "./Recipes";
+
+// import Tasks from "./Tasks";
 
 const App = () => (
   <>
     <nav>
-      <Link to="/">Home</Link> | <Link to="recipes">Recipes</Link> |
-      <Link to="admin">Admin</Link>
+      <Link to="/">Home</Link> | <Link to="recipes">Recipe One</Link> |
+      <Link to="recipeTwo">Recipe Two</Link> | <Link to="admin">Admin</Link>
     </nav>
     <main>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/recipeTwo" element={<RecipeTwo />} />
       </Routes>
     </main>
   </>
@@ -24,14 +27,14 @@ const App = () => (
 const Home = () => (
   <>
     <h1>Sweet Home Kitchen</h1>
-    <RecipeOne />
+    <Recipes />
     {/* <h1>{process.env.REACT_APP_TITLE}</h1>
     <h2>{process.env.REACT_APP_SUBTITLE}</h2> */}
     {/* <Tasks /> */}
   </>
 );
 
-const Recipes = () => (
+const Recipe = () => (
   <>
     <h1>Recipes</h1>
     <p>bdknihfwihfehfewio</p>
