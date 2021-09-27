6f73725e8a77927ff484d5570cd4366dc7ebf5ba
jenhuynh <jendoubleh@gmail.com>
2021-09-27 15:50:19 -0700

    Add endpoints for get request 1, 2, 3

--- server/postRouter.mjs
+++ server/postRouter.mjs
@@ -4,15 +4,34 @@ import * as db from "./db.mjs";
 
 const postRouter = express.Router();
 
-postRouter.get("/", async (request, response) => {
-  const post = await db.getPostOne();
-  response.json(post);
+postRouter.get("/1", async (request, response) => {
+  const posts = await db.getPost1();
+  response.json(posts);
 });
 
-// postRouter.use(express.json());
-// postRouter.post("/", async (request, response) => {
-//   const post = await db.addPost(request.body.name);
-//   response.status(201).json(post);
+postRouter.get("/2", async (request, response) => {
+  const posts = await db.getPost2();
+  response.json(posts);
+});
+
+postRouter.get("/3", async (request, response) => {
+  const posts = await db.getPost3();
+  response.json(posts);
+});
+postRouter.post("/", async (request, response) => {
+  const post = await db.addPost(request.body.name);
+  response.status(201).json(post);
+});
+
+// postRouter.get("/", async (request, response) => {
+//   const post = await db.getPostTwo();
+//   response.json(post);
 // });
+
+// postRouter.get("/", async (request, response) => {
+//   const post = await db.getPostThree();
+//   response.json(post);
+// });
+// postRouter.use(express.json());
 
 export default postRouter;
