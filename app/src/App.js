18b661fb2cf01e34a69bcfdf794b1d3d32a756d2
jenhuynh <jendoubleh@gmail.com>
2021-09-27 19:13:30 -0700

    Add admin and recipeone component

--- app/src/App.js
+++ app/src/App.js
@@ -2,23 +2,24 @@ import * as React from "react";
 
 import { Routes, Route, Link } from "react-router-dom";
 
+import Admin from "./Admin";
+import RecipeOne from "./RecipeOne";
 import RecipeThree from "./RecipeThree";
 import RecipeTwo from "./RecipeTwo";
-import Recipes from "./Recipes";
 
 // import Tasks from "./Tasks";
 
 const App = () => (
   <>
     <nav>
-      <Link to="/">Home</Link> | <Link to="recipes">Recipe One</Link> |
+      <Link to="/">Home</Link> | <Link to="recipeOne">Recipe One</Link> |
       <Link to="recipeTwo">Recipe Two</Link> |{" "}
       <Link to="recipeThree">Recipe Three</Link> | <Link to="admin">Admin</Link>
     </nav>
     <main>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/recipeOne" element={<RecipeOne />} />
         <Route path="/recipeTwo" element={<RecipeTwo />} />
         <Route path="/recipeThree" element={<RecipeThree />} />
         <Route path="/admin" element={<Admin />} />
@@ -30,7 +31,10 @@ const App = () => (
 const Home = () => (
   <>
     <h1>Sweet Home Kitchen</h1>
-    <Recipes />
+    <RecipeOne />
+    <RecipeTwo />
+    <RecipeThree />
+    <Admin />
     {/* <h1>{process.env.REACT_APP_TITLE}</h1>
     <h2>{process.env.REACT_APP_SUBTITLE}</h2> */}
     {/* <Tasks /> */}
@@ -44,11 +48,11 @@ const Recipe = () => (
   </>
 );
 
-const Admin = () => (
-  <>
-    <h1>Admin</h1>
-    <p>bdknihfwihfehfewio</p>
-  </>
-);
+// const Admin = () => (
+//   <>
+//     <h1>Admin</h1>
+//     <p>bdknihfwihfehfewio</p>
+//   </>
+// );
 
 export default App;
