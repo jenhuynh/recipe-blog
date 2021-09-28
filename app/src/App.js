fa05f58d4004a3ed02f3c466cf719c7ccb6e03be
jenhuynh <jendoubleh@gmail.com>
2021-09-28 14:55:37 -0700

    import css files and new recipe components

--- app/src/App.js
+++ app/src/App.js
@@ -7,12 +7,15 @@ import RecipeOne from "./RecipeOne";
 import RecipeThree from "./RecipeThree";
 import RecipeTwo from "./RecipeTwo";
 
-// import Tasks from "./Tasks";
+import "./Recipes.css";
+import "./RecipeOne.css";
+import "./RecipeTwo.css";
+import "./RecipeThree.css";
 
 const App = () => (
   <>
     <nav>
-      <Link to="/">Home</Link> | <Link to="recipeOne">Recipe One</Link> |
+      <Link to="/">Home</Link> | <Link to="recipeOne">Recipe One</Link>|{" "}
       <Link to="recipeTwo">Recipe Two</Link> |{" "}
       <Link to="recipeThree">Recipe Three</Link> | <Link to="admin">Admin</Link>
     </nav>
@@ -31,9 +34,9 @@ const App = () => (
 const Home = () => (
   <>
     <h1>Sweet Home Kitchen</h1>
-    <RecipeOne />
-    <RecipeTwo />
-    <RecipeThree />
+    <Recipe2 />
+    <Recipe1 />
+    <Recipe3 />
     <Admin />
     {/* <h1>{process.env.REACT_APP_TITLE}</h1>
     <h2>{process.env.REACT_APP_SUBTITLE}</h2> */}
@@ -41,6 +44,38 @@ const Home = () => (
   </>
 );
 
+const Recipe1 = () => {
+  return (
+    <div className="recipe1-hero" id="section1">
+      <a href="http://localhost:3000/recipeOne">
+        <h1>Easy Tuna Casserole</h1>
+        <h4>apsreifee</h4>
+      </a>
+    </div>
+  );
+};
+
+const Recipe2 = () => {
+  return (
+    <div className="recipe1-hero" id="section1">
+      <a href="http://localhost:3000/recipeTwo">
+        <h1>Ground Beef Curry Noodle</h1>
+        <h4>apsreifee</h4>
+      </a>
+    </div>
+  );
+};
+
+const Recipe3 = () => {
+  return (
+    <div className="recipe1-hero" id="section1">
+      <a href="http://localhost:3000/recipeThree">
+        <h1>Ground Beef Curry Noodle</h1>
+        <h4>apsreifee</h4>
+      </a>
+    </div>
+  );
+};
 const Recipe = () => (
   <>
     <h1>Recipes</h1>
