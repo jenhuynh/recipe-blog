f08961b1d1b414c0da12e1e0bdade06e4c604729
jenhuynh <jendoubleh@gmail.com>
2021-09-28 17:08:43 -0700

    Map db data on recipetwo page

--- app/src/RecipeTwo.js
+++ app/src/RecipeTwo.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import * as apiClient from "./apiClient";
 
-import "./Recipes.css";
+import "./RecipeTwo.css";
 
 const Recipes = () => {
   const [post, setPost] = React.useState([]);
@@ -21,201 +21,41 @@ const Recipes = () => {
     <>
       {/* <AddRecipeForm /> */}
       <div className="recipe-one" id="sectionOne">
-        {post.map(({ id, title, date, description, steps, ingredients }) => (
-          <li key={id}>
-            <h2>{title}</h2>
-            <h6>{date}</h6>
-            <p>{description}</p>
-            <p>{steps}</p>
-            <p>{ingredients}</p>
-          </li>
-        ))}
+        {post.map(
+          ({
+            id,
+            title,
+            date,
+            description,
+            steps,
+            ingredients,
+            // imageurl,
+            credit,
+          }) => (
+            <li key={id}>
+              <h2>{title}</h2>
+              {/* {imageurl ? (
+                <img
+                  src={`/api/posts/1`}
+                  alt="image of ground beef curly noodle"
+                  style={{
+                    width: 200,
+                    objectFit: "contain",
+                    marginLeft: "1rem",
+                  }}
+                />
+              ) : null} */}
+              <h6>{date}</h6>
+              <p>{description}</p>
+              <p>{steps}</p>
+              <p>{ingredients}</p>
+              <p>{credit}</p>
+            </li>
+          ),
+        )}
       </div>
     </>
   );
 };
-
-// const TaskList = ({ tasks }) => (
-//   <ul>
-//     {tasks.map(({ id, name }) => (
-//       <li key={id}>{name}</li>
-//     ))}
-//   </ul>
-// );
-
-const AddRecipeForm = ({ addPost }) => {
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    //grabbing info into form out of form
-    const {
-      date: { value: date },
-      title: { value: title },
-      description: { value: description },
-      steps: { value: steps },
-      ingredients: { value: ingredients },
-    } = form.elements;
-
-    console.log(title, date, description, steps, ingredients);
-    form.reset();
-
-    addPost({
-      date,
-      title,
-      description,
-      steps,
-      ingredients,
-    });
-  };
-
-  return (
-    <form onSubmit={onSubmit}>
-      <h3>Add new recipe:</h3>
-      <div>
-        <div>
-          <label>
-            <input name="date" type="date" placeholder="Add date" />
-          </label>
-          <label>
-            <input name="title" type="text" placeholder="Add title" />
-          </label>
-          <label>
-            <input
-              name="description"
-              type="text"
-              placeholder="Add description"
-            />
-          </label>
-          <label>
-            <input
-              name="steps"
-              type="text"
-              placeholder="Let us know how to make the dish"
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            <input
-              name="ingredients"
-              type="text"
-              placeholder="List ingredients"
-              required
-            />
-          </label>
-          <button>Add Recipe</button>
-        </div>
-      </div>
-    </form>
-  );
-};
-
-// const Recipes = () => {
-//   const [post, setPost] = React.useState([]);
-
-//   // const [tasks, setTasks] = React.useState([]);
-
-//   const loadPost = async () => setPost(await apiClient.getPost());
-
-//   const addPost = (post) => apiClient.addPost(post).then(loadPost);
-
-//   React.useEffect(() => {
-//     loadPost();
-//   }, []);
-
-//   return (
-//     <>
-//       <AddRecipeForm addPost={addPost} />
-//       <div className="recipe-one" id="sectionOne">
-//         {post.map(({ id, title, date, description, steps, ingredients }) => (
-//           <li key={id}>
-//             <h2>{title}</h2>
-//             <h6>{date}</h6>
-//             <p>{description}</p>
-//             <p>{steps}</p>
-//             <p>{ingredients}</p>
-//           </li>
-//         ))}
-//       </div>
-//     </>
-//   );
-// };
-
-// // const TaskList = ({ tasks }) => (
-// //   <ul>
-// //     {tasks.map(({ id, name }) => (
-// //       <li key={id}>{name}</li>
-// //     ))}
-// //   </ul>
-// // );
-
-// const AddRecipeForm = ({ addPost }) => {
-//   const onSubmit = (event) => {
-//     event.preventDefault();
-//     const form = event.currentTarget;
-//     //grabbing info into form out of form
-//     const {
-//       date: { value: date },
-//       title: { value: title },
-//       description: { value: description },
-//       steps: { value: steps },
-//       ingredients: { value: ingredients },
-//     } = form.elements;
-
-//     console.log(title, date, description, steps, ingredients);
-//     form.reset();
-
-//     addPost({
-//       date,
-//       title,
-//       description,
-//       steps,
-//       ingredients,
-//     });
-//   };
-
-//   return (
-//     <form onSubmit={onSubmit}>
-//       <h3>Add new recipe:</h3>
-//       <div>
-//         <div>
-//           <label>
-//             <input name="date" type="date" placeholder="Add date" />
-//           </label>
-//           <label>
-//             <input name="title" type="text" placeholder="Add title" />
-//           </label>
-//           <label>
-//             <input
-//               name="description"
-//               type="text"
-//               placeholder="Add description"
-//             />
-//           </label>
-//           <label>
-//             <input
-//               name="steps"
-//               type="text"
-//               placeholder="Let us know how to make the dish"
-//               required
-//             />
-//           </label>
-//         </div>
-//         <div>
-//           <label>
-//             <input
-//               name="ingredients"
-//               type="text"
-//               placeholder="List ingredients"
-//               required
-//             />
-//           </label>
-//           <button>Add Recipe</button>
-//         </div>
-//       </div>
-//     </form>
-//   );
-// };
 
 export default Recipes;
