30b00ba76a9bc82fc96057215ad0bee8d685005f
jenhuynh <jendoubleh@gmail.com>
2021-10-01 15:19:41 -0700

    reorder route links and content order

--- app/src/App.js
+++ app/src/App.js
@@ -17,8 +17,8 @@ const App = () => (
     <nav className="sticky">
       <Link to="/">Home</Link>
       <Link to="recipeOne">Easy Tuna Casserole</Link>
-      <Link to="recipeThree">Air Fryer Potatoes</Link>
       <Link to="recipeTwo">Ground Beef Curly Noodle</Link>
+      <Link to="recipeThree">Air Fryer Potatoes</Link>
       <Link to="admin">Admin</Link>
     </nav>
     <main>
@@ -39,8 +39,8 @@ const Home = () => (
       <h1 className="header-txt">Sweet Home Kitchen</h1>
     </div>
     <div className="body">
-      <Recipe2 />
       <Recipe1 />
+      <Recipe2 />
       <Recipe3 />
       <Admin />
     </div>
@@ -53,28 +53,6 @@ const Home = () => (
 const Recipe1 = () => {
   return (
     <>
-      <div className="recipe3-card" id="section1">
-        <a className="recipe-links" href="http://localhost:3000/recipeThree">
-          <h1>Air Fried Potatoes</h1>
-          <h4>
-            These air-fried roasted one-bite potatoes are crispy on the outside
-            and creamy on the inside. Quick and easy to make.
-          </h4>
-        </a>
-        <img
-          src="https://kitchendivas.com/wp-content/uploads/Air-fryer-baby-potatoes-7-1.jpg"
-          alt="image of ground beef curry noodles"
-          height="600px"
-        />
-      </div>
-      <hr />
-    </>
-  );
-};
-
-const Recipe2 = () => {
-  return (
-    <>
       <div className="recipe1-card" id="section1">
         <a href="http://localhost:3000/recipeOne">
           <h1>Easy Tuna Casserole</h1>
@@ -84,11 +62,34 @@ const Recipe2 = () => {
             roommate.{" "}
           </h4>
         </a>
-        <img
-          src="https://www.thekitchenwhisperer.net/wp-content/uploads/2020/02/Hot-Tuna-Noodle-Casserole-1.jpg"
-          alt="image of tuna casserole"
-          height="500px"
-        />
+        <a href="http://localhost:3000/recipeThree">
+          <img
+            src="https://www.thekitchenwhisperer.net/wp-content/uploads/2020/02/Hot-Tuna-Noodle-Casserole-1.jpg"
+            alt="image of tuna casserole"
+            height="500px"
+          />
+        </a>
+      </div>
+      <hr />
+    </>
+  );
+};
+
+const Recipe2 = () => {
+  return (
+    <>
+      <div className="recipe2-card" id="section1">
+        <a href="http://localhost:3000/recipeTwo">
+          <h1>Ground Beef Curry Noodle</h1>
+          <h4>An easy, quick and economical recipe that kids really like!</h4>
+        </a>
+        <a href="http://localhost:3000/recipeTwo">
+          <img
+            src="https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Curly-Noodle-Dinner_exps5896_W1973175D05_03_3bC_RMS.jpg"
+            alt="image of tuna casserole"
+            height="500px"
+          />
+        </a>
       </div>
       <hr />
     </>
@@ -98,16 +99,21 @@ const Recipe2 = () => {
 const Recipe3 = () => {
   return (
     <>
-      <div className="recipe2-card" id="section1">
-        <a href="http://localhost:3000/recipeTwo">
-          <h1>Ground Beef Curry Noodle</h1>
-          <h4>An easy, quick and economical recipe that kids really like!</h4>
+      <div className="recipe3-card" id="section1">
+        <a className="recipe-links" href="http://localhost:3000/recipeThree">
+          <h1>Air Fried Potatoes</h1>
+          <h4>
+            These air-fried roasted one-bite potatoes are crispy on the outside
+            and creamy on the inside. Quick and easy to make.
+          </h4>
         </a>
-        <img
-          src="https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Curly-Noodle-Dinner_exps5896_W1973175D05_03_3bC_RMS.jpg"
-          alt="image of tuna casserole"
-          height="500px"
-        />
+        <a href="http://localhost:3000/recipeThree">
+          <img
+            src="https://kitchendivas.com/wp-content/uploads/Air-fryer-baby-potatoes-7-1.jpg"
+            alt="image of ground beef curry noodles"
+            height="600px"
+          />
+        </a>
       </div>
       <hr />
     </>
